9da137b6dbcaca57580be3b71d11aadfe012f7e7
lavanyaveeramachaneni <ch.lavanya145@gmail.com>
2022-01-30 21:32:31 -0500

    Code changes for asssignment1

--- Assignment_1_Spring_2022/Program.cs
+++ Assignment_1_Spring_2022/Program.cs
@@ -1,10 +1,6 @@
-﻿/* 
- 
-YOU ARE NOT ALLOWED TO MODIFY ANY FUNCTION DEFINATION's PROVIDED.
-WRITE YOUR CODE IN THE RESPECTIVE FUNCTION BLOCK
-
-*/
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DIS_Assignmnet1_SPRING_2022
 {
@@ -67,209 +63,204 @@ namespace DIS_Assignmnet1_SPRING_2022
 
         }
 
-        /* 
-        <summary>
-        Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
-
-        Example 1:
-        Input: s = "MumaCollegeofBusiness"
-        Output: "MmCllgfBsnss"
-
-        Example 2:
-        Input: s = "aeiou"
-        Output: ""
-
-        Constraints:
-        •	0 <= s.length <= 10000
-        s consists of uppercase and lowercase letters
-        </summary>
-        */
-
+        /*Timespent = 25min, learned differnet list operations avaiualble
+         * https://www.tutorialsteacher.com/csharp/csharp-list 
+         https://csharp.net-tutorials.com/collections/lists/ */
         private static string RemoveVowels(string s)
         {
-            try
+            if (s.Length > 0 & s.Length < 10000)//given string length should be >0
             {
-                // write your code here
-                String final_string ="";
-                return final_string;
+                try
+                {
+                    String final_string = "";
+                    //Character list of vowels
+                    var vowels = new List<char>() { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' };
+                    //Iterating over the given string and checking if it has any vowels, if character is not vowel adding to the final_string 
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        if (!vowels.Contains(s[i]))
+                            final_string = final_string + s[i];
+                    }//end of for loop
+                    return final_string;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                    throw;
+                }
             }
-            catch (Exception)
+            else//if string is not in the given length, we are pritnting messgae in console and returning input string
             {
-                throw;
+                Console.WriteLine("Input string is not in the expected range of length");
+                return s;
             }
-
         }
 
-        /* 
-        <summary>
-       Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-       A string is represented by an array if the array elements concatenated in order forms the string.
-       Example 1:
-       Input: : bulls_string1 = ["Marshall", "Student",”Center”], : bulls_string2 = ["MarshallStudent ", "Center"]
-       Output: true
-       Explanation:
-       word1 represents string "marshall" + "student" + “center” -> "MarshallStudentCenter "
-       word2 represents string "MarshallStudent" + "Center" -> "MarshallStudentCenter"
-       The strings are the same, so return true.
-       Example 2:
-       Input: word1 = ["Zimmerman", "School", ”of Advertising”, ”and Mass Communications”], word2 = ["Muma", “College”,"of”, “Business"]
-       Output: false
 
-       Example 3:
-       Input: word1  = ["University", "of", "SouthFlorida"], word2 = ["UniversityofSouthFlorida"]
-       Output: true
-       </summary>
-       */
-
+        /*Timespent =30min, Learned more about different string operations
+         * https://www.completecsharptutorial.com/csharp-articles/csharp-string-function.php
+         */
         private static bool ArrayStringsAreEqual(string[] bulls_string1, string[] bulls_string2)
         {
             try
             {
-                // write your code here.
-                return false;
+                string finalstring1 = string.Empty;
+                string finalstring2 = string.Empty;
+                //Adding 2 string arrays to 2 stirng varaibles i.e finalstring1,finalstring2
+                for (int i = 0; i < bulls_string1.Length; i++)
+                    finalstring1 = finalstring1 + bulls_string1[i];
+                for (int i = 0; i < bulls_string2.Length; i++)
+                    finalstring2 = finalstring2 + bulls_string2[i];
+                //Comparing lengths of 2 strings and checking strings are equal or not with Equal function and returning True,if condition true else retruning False
+                if (finalstring1.Length.Equals(finalstring2.Length) && string.Equals(finalstring1, finalstring2))
+                    return true;
+                else
+                    return false;
             }
-            catch (Exception)
+            catch (Exception e)
             {
-
+                Console.WriteLine(e.Message.ToString());
                 throw;
             }
-
         }
-        /*
-        <summary> 
-       You are given an integer array bull_bucks. The unique elements of an array are the elements that appear exactly once in the array.
-       Return the sum of all the unique elements of nums.
-       Example 1:
-       Input: bull_bucks = [1,2,3,2]
-       Output: 4
-       Explanation: The unique elements are [1,3], and the sum is 4.
-       Example 2:
-       Input: bull_bucks = [1,1,1,1,1]
-       Output: 0
-       Explanation: There are no unique elements, and the sum is 0.
-       Example 3:
-       Input: bull_bucks = [1,2,3,4,5]
-       Output: 15
-       Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
-       </summary>
-        */
 
+
+        /*Timespent =40min, learned about different list operations available
+         * https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-6.0
+         */
         private static int SumOfUnique(int[] bull_bucks)
         {
             try
             {
-                // write your code here
-                return 0;
-
+                List<int> input1 = new List<int>(bull_bucks);
+                List<int> input2 = new List<int>();
+                List<int> input3 = new List<int>();
+                int sum = 0;
+                if (input1.Count > 0 && input1.Count < 100)
+                {
+                    //Adding unique elements to input2 list and repeated elements(repeated from 2nd time) to input3 list
+                    foreach (int ch in input1)
+                    {
+                        if (!input2.Contains(ch))
+                            input2.Add(ch);
+                        else
+                            input3.Add(ch);
+                    }
+                    //removing repated element from input2 list
+                    List<int> nonRepeatedElements = input2.Except(input3).ToList();
+                    //Adding sum of unique elements sum if they are any and returning sum, else returning 0
+                    if (nonRepeatedElements.Count >= 1)
+                    {
+                        foreach (int ch in nonRepeatedElements)
+                            sum = sum + ch;
+                    }
+                }//if ends here              
+                return sum;
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                Console.WriteLine(e.Message.ToString());
                 throw;
             }
         }
-        /*
 
-        <summary>
-       Given a square matrix bulls_grid, return the sum of the matrix diagonals.
-       Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
-
-       Example 1:
-       Input: bulls_grid = [[1,2,3],[4,5,6], [7,8,9]]
-       Output: 25
-       Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
-       Notice that element mat[1][1] = 5 is counted only once.
-       Example 2:
-       Input: bulls_grid = [[1,1,1,1], [1,1,1,1],[1,1,1,1], [1,1,1,1]]
-       Output: 8
-       Example 3:
-       Input: bulls_grid = [[5]]
-       Output: 5
-       </summary>
-
-        */
-
+        /*TimeSpent = 60min, learned about square arrays,
+         * https://www.geeksforgeeks.org/how-to-find-the-length-of-an-array-in-c-sharp/
+         */
         private static int DiagonalSum(int[,] bulls_grid)
         {
             try
             {
-                // write your code here.
-
-                return 0;
+                int arraySize = 0, sum = 0;
+                //finding the dimension of an input array
+                for (int i = bulls_grid.Length / 2; i > 0; i--)
+                {
+                    if (i * i == bulls_grid.Length)
+                    {
+                        arraySize = i;
+                        break;
+                    }
+                }//for loop ends here
+                //Iterating on array and adding diagonal elements and returning sum
+                for (int i = 0; i < arraySize; i++)
+                {
+                    for (int j = 0; j < arraySize; j++)
+                    {
+                        if ((i == j) || ((i + j) == (arraySize - 1)))
+                            sum += bulls_grid[i, j];
+                    }
+                }
+                return sum;
             }
             catch (Exception e)
             {
-
-                Console.WriteLine("An error occured: " + e.Message);
+                Console.WriteLine("An error occured: " + e.Message.ToString());
                 throw;
             }
-
         }
 
 
 
-        /*
-         
-        <summary>
-        Given a string bulls_string  and an integer array indices of the same length.
-        The string bulls_string  will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-        Return the shuffled string.
-
-        Example 3:
-        Input: bulls_string  = "aiohn", indices = [3,1,4,2,0]
-        Output: "nihao"
-
-        */
+        /* TimeSpent = 80Min, Learned more about Linq quiries, 
+         * https://www.tutorialsteacher.com/linq/linq-sorting-operators-orderby-orderbydescending, 
+         * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/sorting-data */
 
         private static string RestoreString(string bulls_string, int[] indices)
         {
+
             try
             {
-                // write your code here.
-                return "null";
+                string restoreString = string.Empty;
+                //checking both given inpjut string and indices lengths are same not and input string length should be 
+                if (bulls_string.Length == indices.Length && indices.Length <= 100)
+                {
+                    Dictionary<int, char> replaceString = new Dictionary<int, char>();
+                    char[] inputCharArray = bulls_string.ToCharArray();
+                    //Manipulating the given input as per the indices
+                    for (int i = 0; i < inputCharArray.Length; i++)
+                        replaceString.Add(indices[i], inputCharArray[i]);
+                    //sorting the manipulated string in ascending oredr with Linq Query
+                    var outputChar = from entry in replaceString orderby entry.Key ascending select entry.Value;
+                    //Adding each charcater to the string
+                    foreach (var ch in outputChar)
+                        restoreString += ch.ToString();
+                }//if ends here
+                return restoreString;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message.ToString());
                 throw;
             }
-
         }
 
-        /*
-         <summary>
-        Given a 0-indexed string bulls_string   and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
-        For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
-        Return the resulting string.
 
-        Example 1:
-        Input: bulls_string   = "mumacollegeofbusiness", ch = "c"
-        Output: "camumollegeofbusiness"
-        Explanation: The first occurrence of "c" is at index 4. 
-        Reverse the part of word from 0 to 4 (inclusive), the resulting string is "camumollegeofbusiness".
-
-        Example 2:
-        Input: bulls_string   = "xyxzxe", ch = "z"
-        Output: "zxyxxe"
-        Explanation: The first and only occurrence of "z" is at index 3.
-        Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
-
-        Example 3:
-        Input: bulls_string   = "zimmermanschoolofadvertising", ch = "x"
-        Output: "zimmermanschoolofadvertising"
-        Explanation: "x" does not exist in word.
-        You should not do any reverse operation, the resulting string is "zimmermanschoolofadvertising".
-        */
+        /* Timespent =25min, learned different operations */
 
         private static string ReversePrefix(string bulls_string6, char ch)
         {
             try
             {
-                String prefix_string ="";
-                return prefix_string;
+                String prefix_string = "";
+                char charToFind = char.ToLower(ch);
+                //Checking given character is available in string or not and input string length should be 250
+                if (bulls_string6.ToLower().Contains(charToFind) && bulls_string6.Length <= 250)
+                {
+                    //storing the index of given character
+                    int specifiedCharIndex = bulls_string6.IndexOf(charToFind);
+                    //Reversing and adding the each character to string from the given character position
+                    for (int i = specifiedCharIndex; i >= 0; i--)
+                        prefix_string += bulls_string6[i];//for loop end here
+                    //Adding the remaining portion of string from the given character index
+                    for (int j = specifiedCharIndex + 1; j < bulls_string6.Length; j++)
+                        prefix_string += bulls_string6[j];
+                    return prefix_string;
+                }//if ends here
+                return bulls_string6;
             }
-            catch (Exception)
+            catch (Exception e)
             {
-
+                Console.WriteLine(e.Message.ToString());
                 throw;
             }
 
